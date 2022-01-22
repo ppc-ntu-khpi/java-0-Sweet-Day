b6930c71be3d9e28f69818d483c79c2a7fd240f7
Duminike Vika <75427769+Sweet-Day@users.noreply.github.com>
2022-01-22 09:55:56 +0000

    Done

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+  String quote = "Прежде всего: будь верен самому себе";
+  String author = "- Вильям Шекспир";
+  public void display() {
+    System.out.println(quote);
+    System.out.println(author);
+  }
+}
